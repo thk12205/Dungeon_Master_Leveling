35390f2871b5a1190f7e71d7216ce0a310612a13
theo <thk12205@gmail.com>
2021-03-19 02:01:46 -0700

    update seed file with valid articles

---
+++ app/controllers/api/articles_controller.rb
@@ -0,0 +1,75 @@
+class Api::ArticlesController < ApplicationController
+
+  def create
+    @article = Article.new(
+      title: params[:title], 
+      url: params[:url], 
+      img_url: params[:img_url],
+      source: params[:source], 
+      category_id: params[:category_id],
+      body: params[:body], 
+      video: params[:video]
+    )
+    if @article.save
+      render "show.json.jb"
+    else
+      render json: { errors: @article.errors.full_messages }, status: :bad_request
+    end
+  end
+
+
+  # get "/users/:id" => "users#show"
+
+  def show
+    @article = Article.find_by(id:params[:id])
+    render "show.json.jb"
+  end
+  
+  # update "/users/:id" => "users#update"
+  
+  def update
+    @article = Article.find_by(id:params[:id])
+    #checks if current user is trying to edit
+    # p "++++++++++++++++++++++++++++++++++++"
+    # p "current_user.id: #{current_user.id}"
+    # p "params[:id].to_i: #{params[:id].to_i}"
+    # unless current_user.id == params[:id].to_i
+    #   #render ends the rest of update's function
+    #   render json: { error: "incorrect user"}, status: :unauthorized
+    # else 
+
+    @article.title = params[:title] || @article.title
+    @article.url = params[:url] || @article.url
+    @article.img_url = params[:img_url] || @article.img_url
+    @article.source = params[:source] || @article.source
+    @article.category_id = params[:category_id] || @article.category_id
+    @article.body = params[:body] || @article.body
+    @article.video = params[:video] || @article.video
+
+    #no email changing
+    #maybe change pw later
+    if @article.save
+      render "show.json.jb"
+    else
+      render json: { errors: @article.errors.full_messages }, status: :bad_request
+    end
+    # end
+  end
+  
+  # destroy "/articles/:id" => "users#destroy"
+  
+  def destroy
+    @article = Article.find_by(id:params[:id])
+    #checks if current user is trying to destroy
+    # p "++++++++++++++++++++++++++++++++++++"
+    # p "current_user.id: #{current_user.id}"
+    # p "params[:id].to_i: #{params[:id].to_i}"
+    # unless current_user.id == params[:id].to_i
+    #   #render ends the rest of update's function
+    #   render json: { error: "incorrect user"}, status: :unauthorized
+    # else
+    @article.destroy
+    render json: { message: "User destroyed successfully~"}
+    # end
+  end
+end
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,5 +4,9 @@ class Article < ApplicationRecord
   has_many :upvotes
   has_many :users, through: :upvotes
 
+  validates :title, presence: true
+  validates :source, presence: true
+  validates :category_id, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 6}
+  validates :video, presence: true
 
 end
