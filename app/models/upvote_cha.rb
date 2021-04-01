777af30f94eb0c74fb98a6d356f09c617af6fa0f
theo <thk12205@gmail.com>
2021-03-31 21:41:23 -0700

    add campaignsession,articlemarker to advanced partial for campaign index and show views

--- app/models/upvote_cha.rb
+++ app/models/upvote_cha.rb
@@ -3,4 +3,19 @@ class UpvoteCha < ApplicationRecord
   belongs_to :user
   belongs_to :article
 
+
+  def top_three_articles
+    articles.sort_by{|article| article.upvotes.count}.reverse.take(3)
+  end
+
+  def weighted_score
+    gold = 0
+    articles.users.map do |user|
+      user_stat = user.cha
+      user.upvotes.map do |upvote|
+        gold += user_stat
+      end
+    end
+  end
+
 end
